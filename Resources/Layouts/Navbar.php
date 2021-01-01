40339ffd2414e6215299a97017250e9e567b3d34
Shreekant Shinde <73630667+Shreekant-Shinde@users.noreply.github.com>
2021-01-01 09:54:21 +0530

    Changed directory structure
    also made changes to contact us form
    added css to style.css
    made seperate file for navbar and footer
    used .php extension to file replacing .html , so we can use php function in same file

---
+++ Resources/Layouts/Navbar.php
@@ -0,0 +1,22 @@
+<navbar class="navbar">
+      <a href="index.html">Home</a>
+
+      <a href="aboutus.html">About Us</a>
+
+      <div class="dropdown">
+            <button class="dropbtn">Brain Games
+                  <i class="down"></i>
+            </button>
+
+            <div class="dropdown-content">
+                  <a href="braingames.html">Typing Test</a>
+                  <a href="braingames.html">Situation Test</a>
+                  <a href="braingames.html">Memory Game</a>
+                  <!--<a href="#">Sudoku</a>    optional-->
+            </div>
+      </div>
+
+      <a href="funfact.html">Fun Facts</a>
+
+      <a href="contactus.html">Contact Us</a>
+</navbar>
