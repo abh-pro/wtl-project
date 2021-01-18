b152a4b93258e4d32b21c5adb66328315eed462c
Shreekant Shinde <73630667+Shreekant-Shinde@users.noreply.github.com>
2021-01-18 12:25:39 +0530

    Changes to directory updated

---
+++ Resources/Views/AboutUs.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en-US">
+      <head>
+            <meta charset="utf-8">
+
+            <title>Brain Games</title>
+
+            <link rel="stylesheet" type="text/css" href="../css/style.css">
+      </head>
+
+      <body>
+            <header>
+                  <?php include_once('../Layouts/Navbar.php') ?>
+
+                  <div class="bgimg1">
+                        <img src="..\css\img\denise-jans-HoqYAnwR-1g-unsplash.jpg">
+                  </div>
+            </header>
+
+            <main>
+
+            </main>
+
+            <footer>
+                  <i>Made in conjunction with abhishek,diksha,mitesh,prathamesh,shreekant.</i>
+            </footer>
+      </body>
+</html>
